c2dc772e11373c10c8ee53b8cf51d13ce148b067
coreyc <ccleary00@gmail.com>
2017-08-13 11:03:40 -0500

    Playback demo working

---
+++ public/playback.js
@@ -0,0 +1,26 @@
+const sequence = () => { }
+
+const createActions = () => { }
+
+const setTransitions = () => { }
+
+setTimeout(() => {
+  document.getElementById('buttonToClick').focus()
+}, 3000)
+
+setTimeout(() => {
+  document.getElementById('pb_cursor').style.left = '25px'
+  document.getElementById('pb_cursor').style.top = '25px'
+}, 3000)
+
+setTimeout(() => {
+  document.getElementById('pb_cursor').style.left = '5px'
+  document.getElementById('pb_cursor').style.top = '5px'
+}, 4000)
+
+setTimeout(() => {
+  document.getElementById('pb_cursor').style.left = '55px'
+  document.getElementById('pb_cursor').style.top = '55px'
+}, 6000)
+
+sequence()
--- src/recorder/server/persistence/db.js
+++ src/recorder/server/persistence/db.js
@@ -1,33 +1,33 @@
 const MongoClient = require('mongodb').MongoClient
-const assert = require('assert')
+const url = 'mongodb://localhost:27017/myproject'
 
 // client ajax post to server, server stores data in db (or other persistence store)
 
-const insert = (db, callback) => {
+const insert = (db, data, callback) => {
   const collection = db.collection('documents')
-  
-  collection.insertMany([
-    {a : 1}, {a : 2}, {a : 3}
-  ], (err, result) => {
-    assert.equal(err, null)
-    assert.equal(3, result.result.n)
-    assert.equal(3, result.ops.length)
-    console.log("Inserted 3 documents into the document collection")
-    callback(result)
+
+  collection.insert(data)
+    .then(result => result)
+    .catch(e => e)
+  // , (err, result) => {
+  //   callback(result)
+  // })
+}
+
+const connectInsert = data => {
+  return MongoClient.connect(url).then(db => {
+    insert(db, data, () => {
+      db.close()
+    })
   })
 }
 
-const url = 'mongodb://localhost:27017/myproject';
-
-MongoClient.connect(url, (err, db) => {
-  assert.equal(null, err)
-  console.log("Connected correctly to server")
-
-  insertDocuments(db, () => {
-    db.close()
-  })
-})
+// , (err, db) => {
+//   insertDocuments(db, () => {
+//     db.close()
+//   })
 
 export {
-  insert
+  insert,
+  connectInsert
 }
